5825ae00667c1f856a5d70be6120386fcac8baa7
dravinanshuMishra <dravinanshu97@gmail.com>
2025-09-08 13:10:04 +0530

    add

---
+++ 2-YearsExperienceJS/ch-2-250+-Questions/script1.js
@@ -0,0 +1,6 @@
+console.log("Script1 file loaded");
+import { var2 } from "./script2.js";
+console.log(var2);
+
+var x = 10;
+console.log(x);
---
+++ 2-YearsExperienceJS/ch-2-250+-Questions/script2.js
@@ -0,0 +1,3 @@
+console.log("Script2 file loaded");
+let var2 = "I am var2 from script2.js";
+export { var2 };
